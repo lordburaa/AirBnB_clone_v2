1650e894cbe4d4cd4c29b25c91d22e27f33999dc
lordburaa <comrdebiruk97@gmail.com>
2024-07-05 22:59:59 -0700

    fix the single and double qoute

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Console Module """
 import cmd
+import json
 import sys
 from models.base_model import BaseModel
 from models.__init__ import storage
@@ -73,7 +74,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,14 +116,35 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
+        list_t = args.split(' ')
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif list_t[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+        
+        new_instance = HBNBCommand.classes[list_t[0]]()
+        i = 1
+        dictionary = {}
+        print(list_t)
+        #for key in range(1, len(list_t)):
+        #   list_t[key] = list_t[key].replace('\'', '\"')
+            
+        print(list_t)
+        i = 1
+        key = 1
+        dic_t = new_instance.to_dict()
+        for key in range(1, len(list_t)):
+            attr_name, attr_value = list_t[key].split('=')
+            if (attr_value.startswith('"') and attr_value.endswith('"')) or (attr_value.startswith("'") and attr_value.endswith("'")):
+                attr_value = attr_value[1:-1]
+
+
+            setattr(new_instance, attr_name, attr_value)
+            i = i + 1
+
+        storage.new(new_instance)
         print(new_instance.id)
         storage.save()
 
@@ -272,7 +294,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +302,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
