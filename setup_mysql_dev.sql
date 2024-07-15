0da06918546ce79e03089e66c5ae0e0f637297c6
lordburaa <comrdebiruk97@gmail.com>
2024-07-15 04:03:01 -0700

    add privielege :

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,10 @@
 -- setup MYSQL server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- comment
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- commnet
 GRANT ALL ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+-- c0mmnet
 GRANT SELECT ON `performanc_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
 
