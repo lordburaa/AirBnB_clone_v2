b03951000dad03e2a967f3acebbeae61635e7bb7
lordburaa <comrdebiruk97@gmail.com>
2024-07-15 04:33:38 -0700

    remove the tilde

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,8 @@
 -- setup MYSQL server
 -- cpmment
-CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
--- comment
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- commnet
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
--- c0mmnet
-GRANT SELECT ON `performanc_schema`.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performanc_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
 
