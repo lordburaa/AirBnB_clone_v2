42d2a536731925714c4940da651e7b0f13398bd4
lordburaa <comrdebiruk97@gmail.com>
2024-07-15 04:21:17 -0700

    adding privelelge

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- comment
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- commnet
-GRANT ALL ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 -- c0mmnet
 GRANT SELECT ON `performanc_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
